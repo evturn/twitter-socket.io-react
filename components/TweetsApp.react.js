39791f9d8e1f54062db9de6c3ba06de234205148
Evan Turner <evturn@gmail.com>
2015-03-02 20:50:39 -0500

    getting initial component state

--- components/TweetsApp.react.js
+++ components/TweetsApp.react.js
@@ -1,4 +1,25 @@
 module.exports = TweetsApp = React.createClass({
+
+  // Set the initial component state
+  getInitialState: function(props){
+    props = props || this.props;
+
+    // Set initial application state using props
+    return {
+      tweets: props.tweets,
+      count: 0,
+      page: 0,
+      paging: false,
+      skip: 0,
+      done: false
+    };
+
+  },
+
+  componentWillReceiveProps: function(newProps, oldProps){
+    this.setState(this.getInitialState(newProps));
+  },
+
   render: function() {
     return (
       <div className="tweets-app">
