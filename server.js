f657fc074cfd08fc8c6552ba87f13f48dc6858b6
Evan Turner <evturn@gmail.com>
2015-03-02 19:47:20 -0500

    twitter and socket.io brought into project

--- server.js
+++ server.js
@@ -10,8 +10,30 @@ var streamHandler = require('./utils/streamHandler');
 var app = express();
 var port = process.envPORT || 8080;
 
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Disable headers
 app.disable('etag');
+
+// Twitter
+var twit = new twitter(config.twitter);
+
+
+app.get('/', routes.index);
+
+app.get('/page/:page/:skip', routes.page);
+
+app.use('/', express.static(__dirname + '/public/'));
+
 
 var server = http.createServer(app).listen(port, function() {
 	console.log('Express listening on port ' + port);
 })
+
+var io = require('socket.io').listen(server);
+
+twit.stream('statuses/filter',{ track: 'isomorphic', '#isomorphic'}, function(stream) {
+  streamHandler(stream,io);
+});
