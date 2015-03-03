0348dcb37d45e6b8074413293b387f78918264eb
Evan Turner <evturn@gmail.com>
2015-03-02 20:56:10 -0500

    added component function to add tweet to queue

--- components/TweetsApp.react.js
+++ components/TweetsApp.react.js
@@ -1,5 +1,24 @@
 module.exports = TweetsApp = React.createClass({
 
+  addTweet: function(tweet){
+
+    // Current application state
+    var updated = this.state.tweets;
+
+    // Increment unread count
+    var count = this.state.count + 1;
+
+    // Increment skip count
+    var skip = this.state.skip + 1;
+
+    // Add tweet to the beginning of tweets array
+    updated.unshift(tweet);
+
+    // Set application state
+    this.setState({tweets: updated, count: count, skip: skip});
+
+  },
+
   // Set the initial component state
   getInitialState: function(props){
     props = props || this.props;
@@ -18,6 +37,20 @@ module.exports = TweetsApp = React.createClass({
 
   componentWillReceiveProps: function(newProps, oldProps){
     this.setState(this.getInitialState(newProps));
+  },
+
+  // Called on client after component rendering
+  componentDidMount: function(){
+    var self = this;
+    var socket = io.connect();
+
+    // On tweet event emission add tweet to queue
+    socket.on('tweet', function (data) {
+      self.addTweet(data);
+    });
+
+    // Infinite scroll
+    window.addEventListener('scroll', this.checkWindowScroll);
   },
 
   render: function() {
