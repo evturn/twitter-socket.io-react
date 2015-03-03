360b49c116df042ccc5498ba7b916c5cb3f34fa2
Evan Turner <evturn@gmail.com>
2015-03-02 20:21:39 -0500

    routes requiring React components

---
+++ routes.js
@@ -0,0 +1,5 @@
+var JSX       = require('node-jsx').install(),
+var React     = require('react'),
+var TweetsApp = require('./components/TweetsApp.react'),
+var Tweet     = require('./models/Tweet');
+
