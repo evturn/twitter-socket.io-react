baed9d7462a66831f87cb9f2a4934186bfc234fe
Evan Turner <evturn@gmail.com>
2015-03-02 20:07:24 -0500

    method created to return tweet data from the db

--- models/Tweet.js
+++ models/Tweet.js
@@ -9,3 +9,22 @@ var schema = new mongoose.Schema({
 	date	 		: Data,
 	screename : String	
 });
+
+schema.statics.getTweets = function(page, skip, callback) {
+	
+	// Return tweets from DB
+	var tweets = [];
+			start	 = (page * 10) + (skip * 1);
+
+	// Query the DB
+  Tweet.find({},'twid active author avatar body date screenname',{skip: start, limit: 10}).sort({date: 'desc'}).exec(function(err,docs){
+
+    if(!err) {
+      tweets = docs;
+      tweets.forEach(function(tweet){
+        tweet.active = true;
+      });
+    }
+    callback(tweets);
+  });
+};
