0cd4b8e369744545c2011aaf9659c67f9361b68a
Evan Turner <evturn@gmail.com>
2015-03-02 20:25:47 -0500

    index route written to render home template and stringify React component

--- routes.js
+++ routes.js
@@ -3,3 +3,29 @@ var React     = require('react'),
 var TweetsApp = require('./components/TweetsApp.react'),
 var Tweet     = require('./models/Tweet');
 
+module.exports = {
+
+  index: function(req, res) {
+   
+    // Schema method to get tweets from DB
+    Tweet.getTweets(0,0, function(tweets, pages) {
+
+      // Render React to a string with tweets
+      var markup = React.renderComponentToString(
+        TweetsApp({
+          tweets: tweets
+        })
+      );
+
+      // Render Home with React markup and browser state
+      res.render('home', {
+        markup: markup,
+        state: JSON.stringify(tweets) 
+      });
+
+    });
+  },
+
+  
+
+}
